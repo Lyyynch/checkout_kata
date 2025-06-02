1889bfa1a0726150e61b27af167af696168e16a3
Michael Lynch <m.lynch96@googlemail.com>
2025-06-02 20:52:31 +0100

    Refactored SkuRetrievalServiceStub.cs

--- CheckoutKata.Tests/Stubs/SkuRetrievalServiceStub.cs
+++ CheckoutKata.Tests/Stubs/SkuRetrievalServiceStub.cs
@@ -1,21 +1,52 @@
+using static CheckoutKata.SpecialType;
+
 namespace CheckoutKata.Tests.Stubs;
 
 public class SkuRetrievalServiceStub : ISkuRetrievalService
 {
-    private readonly List<Sku> _defaultItemList =
+    private readonly List<Sku> _skuList =
     [
-        new Sku("A", 50, new SkuSpecial(3, 20)),
-        new Sku("B", 30, new SkuSpecial(2, 15)),
-        new Sku("C", 20),
-        new Sku("D", 15),
-        new Sku("E", 45, new SkuSpecial(4, 45)),
-        new Sku("F", 50, new SkuSpecial(2, 30)),
-        new Sku("G", 30, new SkuSpecial(4, 30, 1)),
-        new Sku("H", 60, new SkuSpecial(4, 60, 3))
+        new("A", 50),
+        new("B", 30),
+        new("C", 20),
+        new("D", 15),
+        new("E", 45),
+        new("F", 50),
+        new("G", 30),
+        new("H", 60)
     ];
+    
+    private readonly List<List<int?>?> _skuSpecialList =
+    [
+        [0, 3, 20],
+        [0, 2, 15],
+        [],
+        [],
+        [1, 4, 45],
+        [2, 2, 30],
+        [1, 4, 30, 1],
+        [0, 4, 60, 3]
+    ];
+
+    public SkuRetrievalServiceStub()
+    {
+        for (var i = 0; i < _skuList.Count; i++)
+        {
+            if (_skuSpecialList[i]?.Count >= 3)
+            {
+                var specialArgs = _skuSpecialList[i];
+                _skuList[i].CreateSpecial(
+                    (SpecialType)specialArgs[0]!.Value, 
+                    specialArgs[1]!.Value, 
+                    specialArgs[2]!.Value, 
+                    specialArgs.Count > 3 ? specialArgs[3] : null
+                );
+            }
+        }
+    }
     
     public Sku GetSkuByCode(string skuCode)
     {
-        return _defaultItemList.First(sku => sku.Code == skuCode);
+        return _skuList.First(sku => sku.Code == skuCode);
     }
 }
